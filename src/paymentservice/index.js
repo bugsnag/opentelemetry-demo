3a5e685d220854bff38d377cba1ff730ab299c2d
adolfo.lopez <adolfo.lopez@smartbear.com>
2024-12-16 09:25:33 +0100

    Add bugsnag error monitoring

--- src/paymentservice/index.js
+++ src/paymentservice/index.js
@@ -1,59 +1,82 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
-const grpc = require('@grpc/grpc-js')
-const protoLoader = require('@grpc/proto-loader')
-const health = require('grpc-js-health-check')
-const opentelemetry = require('@opentelemetry/api')
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const health = require("grpc-js-health-check");
+const opentelemetry = require("@opentelemetry/api");
 
-const charge = require('./charge')
-const logger = require('./logger')
+const charge = require("./charge");
+const logger = require("./logger");
+const { trace, context } = require("@opentelemetry/api");
+const Bugsnag = require("@bugsnag/js");
+console.log(process.env.PAYMENT_SERVICE_BUGSNAG_API_KEY);
+
+Bugsnag.start({
+  apiKey: process.env.PAYMENT_SERVICE_BUGSNAG_API_KEY,
+  onError: function (event) {
+    const spanContext = trace.getSpanContext(context.active());
+    event.addMetadata("correlation", {
+      traceId: spanContext.traceId,
+      spanId: spanContext.spanId,
+    });
+  },
+});
 
 async function chargeServiceHandler(call, callback) {
   const span = opentelemetry.trace.getActiveSpan();
 
   try {
-    const amount = call.request.amount
+    const amount = call.request.amount;
     span.setAttributes({
-      'app.payment.amount': parseFloat(`${amount.units}.${amount.nanos}`)
-    })
-    logger.info({ request: call.request }, "Charge request received.")
+      "app.payment.amount": parseFloat(`${amount.units}.${amount.nanos}`),
+    });
+    logger.info({ request: call.request }, "Charge request received.");
 
-    const response = await charge.charge(call.request)
-    callback(null, response)
-
+    const response = await charge.charge(call.request);
+    callback(null, response);
   } catch (err) {
-    logger.warn({ err })
+    logger.warn({ err });
 
-    span.recordException(err)
-    span.setStatus({ code: opentelemetry.SpanStatusCode.ERROR })
+    span.recordException(err);
+    span.setStatus({ code: opentelemetry.SpanStatusCode.ERROR });
 
-    callback(err)
+    callback(err);
   }
 }
 
 async function closeGracefully(signal) {
-  server.forceShutdown()
-  process.kill(process.pid, signal)
+  server.forceShutdown();
+  process.kill(process.pid, signal);
 }
 
-const otelDemoPackage = grpc.loadPackageDefinition(protoLoader.loadSync('demo.proto'))
-const server = new grpc.Server()
+const otelDemoPackage = grpc.loadPackageDefinition(
+  protoLoader.loadSync("demo.proto")
+);
+const server = new grpc.Server();
 
-server.addService(health.service, new health.Implementation({
-  '': health.servingStatus.SERVING
-}))
+server.addService(
+  health.service,
+  new health.Implementation({
+    "": health.servingStatus.SERVING,
+  })
+);
 
-server.addService(otelDemoPackage.oteldemo.PaymentService.service, { charge: chargeServiceHandler })
+server.addService(otelDemoPackage.oteldemo.PaymentService.service, {
+  charge: chargeServiceHandler,
+});
 
-server.bindAsync(`0.0.0.0:${process.env['PAYMENT_SERVICE_PORT']}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
-  if (err) {
-    return logger.error({ err })
+server.bindAsync(
+  `0.0.0.0:${process.env["PAYMENT_SERVICE_PORT"]}`,
+  grpc.ServerCredentials.createInsecure(),
+  (err, port) => {
+    if (err) {
+      return logger.error({ err });
+    }
+
+    logger.info(`PaymentService gRPC server started on port ${port}`);
+    server.start();
   }
+);
 
-  logger.info(`PaymentService gRPC server started on port ${port}`)
-  server.start()
-}
-)
-
-process.once('SIGINT', closeGracefully)
-process.once('SIGTERM', closeGracefully)
+process.once("SIGINT", closeGracefully);
+process.once("SIGTERM", closeGracefully);
