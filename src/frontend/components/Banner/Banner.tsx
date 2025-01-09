bcbbd9dbebcb9a32f6bad8197e2809d9807427e6
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-01-09 14:28:41 +0100

    deleted manual error

--- src/frontend/components/Banner/Banner.tsx
+++ src/frontend/components/Banner/Banner.tsx
@@ -3,13 +3,8 @@
 
 import Link from 'next/link';
 import * as S from './Banner.styled';
-import Bugsnag from '@bugsnag/js';
 
 const Banner = () => {
-  const onButtonClick = () => {
-    Bugsnag.notify(new Error('Test error'));
-  };
-
   return (
     <S.Banner>
       <S.ImageContainer>
@@ -17,7 +12,7 @@ const Banner = () => {
       </S.ImageContainer>
       <S.TextContainer>
         <S.Title>The best telescopes to see the world closer</S.Title>
-        <Link href="#hot-products" onClick={onButtonClick}>
+        <Link href="#hot-products">
           <S.GoShoppingButton>Go Shopping</S.GoShoppingButton>
         </Link>
       </S.TextContainer>
